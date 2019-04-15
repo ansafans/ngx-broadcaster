efc1e06e82dcf43e3e13d7a2e4024809ba96e324
ansafans <ansafsah@gmail.com>
2019-04-15 15:04:09 +0530

    Implement test for Ng-Broadcaster component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgBroadcasterService } from 'projects/ng-broadcaster/src/public_api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-broadcaster-lib';
+
+  message:string;
+
+  constructor(private broadcaster:NgBroadcasterService){}
+  
+  ngOnInit(){
+    this.broadcaster.listen('test-event').subscribe(res=>{
+      console.log(res);
+      this.message = res.message;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgBroadcasterModule } from 'projects/ng-broadcaster/src/public_api';
+import { SampleComponent } from './sample/sample/sample.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgBroadcasterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
